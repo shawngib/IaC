3d0971312cfef1537276f72cc372769c4f6de300
Shawn Gibbs <shawngib@microsoft.com>
2020-08-29 19:36:28 -0400

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,5 @@
+apt install docker.io -y
+docker pull flatsatcr.azurecr.us/frame-grabber
+docker tag flatsatcr.azurecr.us/frame-grabber flatsatgovacr.azurecr.us/frame-grabber
+docker login flatsatgovacr.azurecr.us -u flatsatgovacr -p 9hDYSTXNI+5AXHBjuTilwtshifk4HOqH
+docker push flatsatgovacr.azurecr.us/frame-grabber
