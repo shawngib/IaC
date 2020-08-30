bd52eb0ffad7eb5ac87cbd7a2ff22d5e41a0b0b7
Shawn Gibbs <shawngib@microsoft.com>
2020-08-29 22:22:25 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,3 +3,4 @@ docker pull flatsatcr.azurecr.us/frame-grabber
 docker tag flatsatcr.azurecr.us/frame-grabber flatsatgovacr.azurecr.us/frame-grabber
 docker login flatsatgovacr.azurecr.us -u flatsatgovacr -p 9hDYSTXNI+5AXHBjuTilwtshifk4HOqH
 docker push flatsatgovacr.azurecr.us/frame-grabber
+printf '{"Docker": "complete"}'
