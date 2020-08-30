505a149096b18eb923da1e5740e0561a47ec7356
Shawn Gibbs <shawngib@microsoft.com>
2020-08-29 23:16:18 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@ result2=$(docker pull flatsatcr.azurecr.us/frame-grabber)
 docker tag flatsatcr.azurecr.us/frame-grabber flatsatgovacr.azurecr.us/frame-grabber
 docker login flatsatgovacr.azurecr.us -u flatsatgovacr -p 9hDYSTXNI+5AXHBjuTilwtshifk4HOqH
 result3=$(docker push flatsatgovacr.azurecr.us/frame-grabber)
-echo {"Install": "$result1", "Pull": "$result2", "push": "$result3"} > $AZ_SCRIPTS_OUTPUT_PATH
+echo {"Install": "$result1", "Pull": "$result2", "push": "$result3"} | jq > $AZ_SCRIPTS_OUTPUT_PATH
 printf '{"Install": "%s", "Pull": "%s", "push":"%s"}' "$result1" "$result2" "$result3"
