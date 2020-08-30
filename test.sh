1b996d8c5bac8467852ece275442d02261b2ae8e
Shawn Gibbs <shawngib@microsoft.com>
2020-08-29 22:28:58 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-apt install docker.io -y
+result=$(apt install docker.io -y)
 docker pull flatsatcr.azurecr.us/frame-grabber
 docker tag flatsatcr.azurecr.us/frame-grabber flatsatgovacr.azurecr.us/frame-grabber
 docker login flatsatgovacr.azurecr.us -u flatsatgovacr -p 9hDYSTXNI+5AXHBjuTilwtshifk4HOqH
 docker push flatsatgovacr.azurecr.us/frame-grabber
-printf '{"Docker": "complete"}'
+printf '{"Docker": "$result"}'
